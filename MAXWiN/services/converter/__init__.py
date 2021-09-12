6aa964c2681b9c649e54b240549b80e0a28d7ec9
MAXWiN <76895322+maxwinmodz@users.noreply.github.com>
2021-09-12 09:08:19 +0530

    Update __init__.py

--- MAXWiN/services/converter/__init__.py
+++ MAXWiN/services/converter/__init__.py
@@ -1,1 +1,10 @@
+from os import listdir
+from os import mkdir
+
+if 'raw_files' not in listdir():
+    mkdir('raw_files')
+
+from MAXWiN.services.converter.converter import convert
+
+__all__ = ["convert"]
 
