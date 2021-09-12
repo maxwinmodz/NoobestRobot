197720c42de26f9f2af74bc0273d2808bdb0ef21
MAXWiN <76895322+maxwinmodz@users.noreply.github.com>
2021-09-12 09:09:30 +0530

    Update __init__.py

--- MAXWiN/services/queues/__init__.py
+++ MAXWiN/services/queues/__init__.py
@@ -1,1 +1,8 @@
+from MAXWiN.services.queues.queues import clear 
+from MAXWiN.services.queues.queues import get
+from MAXWiN.services.queues.queues import is_empty
+from MAXWiN.services.queues.queues import put
+from MAXWiN.services.queues.queues import task_done
+
+__all__ = ["clear", "get", "is_empty", "put", "task_done"]
 
